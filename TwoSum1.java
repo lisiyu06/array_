5bad14babde478d753c8154f5bf61b3daa59ca67
lisiyu06 <18392609095@163.com>
2020-01-10 19:43:29 +0800

    1两数之和

---
+++ TwoSum1.java
@@ -0,0 +1,15 @@
+/**
+ * Author: lisiyu
+ * Created: 2020/1/10
+ */
+public class TwoSum1 {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j=i+1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+    }
+}
