d958f5c996c2a1653c901cacf5b5dbcead85c8ab
lisiyu06 <18392609095@163.com>
2020-01-19 18:01:24 +0800

    移除元素

--- RemoveElement27.java
+++ RemoveElement27.java
@@ -8,10 +8,9 @@
     // 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
     // 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
 
+public class RemoveElement27 {
     // 时间复杂度 O(n) : i 和 j 至少遍历 2n 步
     // 空间复杂度 O(1) : 没有使用额外的数组空间
-
-public class RemoveElement27 {
     public int removeElement1(int[] nums, int val) {
         int i = 0;
         for (int j = 0; j < nums.length; j++) {
@@ -23,6 +22,8 @@ public class RemoveElement27 {
         return i;
     }
 
+    // 时间复杂度 O(n) : i 和 n 最多遍历 n 次
+    // 空间复杂度 O(1) : 没有使用额外的数组空间
     public int removeElement2(int[] nums, int val) {
         int i = 0;
         int n = nums.length;
