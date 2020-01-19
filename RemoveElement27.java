7bcc83905be441e3cfde0178bfc1cc3a1cbdca24
lisiyu06 <18392609095@163.com>
2020-01-19 17:10:45 +0800

    移除元素

--- RemoveElement27.java
+++ RemoveElement27.java
@@ -1,12 +1,25 @@
 /**
  * Author: lisiyu
- * Created: 2020/1/16
+ * Created: 2020/1/19
  */
 
-//
+// LeetCode 27 ----- 移除元素
+    // 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+    // 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+    // 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+    // 时间复杂度 O(n) : i 和 j 至少遍历 2n 步
+    // 空间复杂度 o(1) : 没有使用额外的数组空间
 
 public class RemoveElement27 {
     public int removeElement(int[] nums, int val) {
-
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] !=val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
