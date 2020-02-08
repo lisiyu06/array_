a61098c4643f5bb98e4a9d3d4cf293cb37d1b13b
lisiyu06 <18392609095@163.com>
2020-02-08 20:07:15 +0800

    219 存在重复元素

---
+++ ContainsNearbyDuplicate219.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Author: lisiyu
+ * Created: 2020/2/7
+ */
+
+// LeetCode 219 ----- 存在重复元素
+    // 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，
+    // 使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+
+public class ContainsNearbyDuplicate219 {
+
+    // 哈希表
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
