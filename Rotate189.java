a0b70bc14d12d3c78ce3d351a063957e235103f5
lisiyu06 <18392609095@163.com>
2020-02-10 21:51:13 +0800

    189 旋转数组

---
+++ Rotate189.java
@@ -0,0 +1,30 @@
+/**
+ * Author: lisiyu
+ * Created: 2020/2/10
+ */
+
+// LeetCode 189 ----- 旋转数组
+    // 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+    // 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+    // 要求使用空间复杂度为 O(1) 的 原地 算法。
+
+public class Rotate189 {
+
+    // 法 1：直接遍历
+    // 时间复杂度：O(n*k)
+    // 空间复杂度：O(1)
+    public void rotate(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int cur = nums[nums.length - 1];
+            for (int j = 0; j < nums.length;j++) {
+                int temp = nums[j];
+                nums[j] = cur;
+                cur = temp;
+
+            }
+        }
+    }
+
+    // 法 2：
+
+}
